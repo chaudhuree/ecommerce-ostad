94a98e85d95b2ba731330cc1a51682f861ed117f
chaudhuree <chaudhuree00@gmail.com>
2023-02-18 21:15:46 +0600

    5️⃣ setup react router dom & install bootstrap cdn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 function App() {
   
 
   return (
     <div className="App">
-      <h1>client side</h1>
+     <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+        </Routes>
+     </Router>
     </div>
   )
 }
