b610d8bf70f7d33cd9d3a04ad6c5d7a18c0de1f3
chaudhuree <chaudhuree00@gmail.com>
2023-02-21 03:20:29 +0600

    docs: create auth context and access its value in Home.jsx

---
+++ client/src/context/auth.jsx
@@ -0,0 +1,24 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [auth,setAuth]=useState({
+    user:'sohan',
+    // user:null,
+    token:"",
+  })
+  
+  return (
+    <AuthContext.Provider value={{auth,setAuth}}>
+      {children}
+    </AuthContext.Provider>
+  )
+  
+  
+  ;}
+
+const useAuth = () => useContext(AuthContext);
+// const {auth,setAuth}=useAuth()
+export { AuthProvider, useAuth };
+
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import Jumbotron from '../components/cards/Jumbotron'
+import React from 'react';
+import Jumbotron from '../components/cards/Jumbotron';
+import { useAuth } from '../context/auth';
 export default function Home() {
+  const {auth,setAuth}=useAuth()
+  console.log(auth);
   return (
     <div>
       <Jumbotron title="Home" subtitle="Book World"></Jumbotron>
+      <h1>name:{auth?.user}</h1>
     </div>
   )
 }
