accd3b2ea2ef46f7af379796acaf128823a780ea
chaudhuree <chaudhuree00@gmail.com>
2023-03-20 17:57:08 +0600

    2️⃣7️⃣ user dashboard design done  with two page UserProfile and UserOrders

--- client/src/pages/user/Dashboard.jsx
+++ client/src/pages/user/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Jumbotron from '../../components/cards/Jumbotron'
+import UserMenu from '../../components/nav/UserMenu'
 import { useAuth } from '../../context/auth'
 
 export default function Dashboard() {
@@ -9,6 +10,22 @@ export default function Dashboard() {
     <Jumbotron
    title={`Hello ${auth?.user?.name}`}
    subtitle="User Dashboard"
- /></>
+ />
+ <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-3">
+            <UserMenu />
+          </div>
+          <div className="col-md-9">
+            <div className="p-3 mt-2 mb-2 h4 bg-light">User Information</div>
+
+            <ul className="list-group">
+              <li className="list-group-item">{auth?.user?.name}</li>
+              <li className="list-group-item">{auth?.user?.email}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+ </>
   )
 }
